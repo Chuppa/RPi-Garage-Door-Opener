9081006f896b4643b28f45c660e7db4ecf2c05da
Chuppa <steve@chuppa.com.au>
2020-01-25 00:27:40 +1100

    Update index.js

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ module.exports = (homebridge) => {
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
 
-  homebridge.registerAccessory('homebridge-rpi-garage-door-opener', 'RPiGarageDoorOpener', RPiGarageDoorOpener);
+  homebridge.registerAccessory('rpi-garage-door-opener', 'rpi-garage-door-opener', rpi-garage-door-opener);
 };
 
-class RPiGarageDoorOpener {
+class rpi-garage-door-opener {
   constructor (log, config) {
 
     //get config values
